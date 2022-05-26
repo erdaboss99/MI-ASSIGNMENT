a73200d5e896711f897e543a37657d8e62e07741
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-05-27 01:02:32 +0200

    Create GUI

--- Project/MestIntBeadando/Form1.cs
+++ Project/MestIntBeadando/Form1.cs
@@ -1,10 +1,75 @@
+using MestIntBeadando.AllapotTer;
+
 namespace MestIntBeadando
 {
     public partial class Form1 : Form
     {
+        Mezo[,] gameField = new Mezo[Allapot.ROW_COUNT, Allapot.COLUMN_COUNT];
+        Allapot allapot;
+
         public Form1()
         {
+            allapot = new Allapot();
             InitializeComponent();
+            for (int i = 0; i < Allapot.ROW_COUNT; i++)
+            {
+                for (int j = 0; j < Allapot.COLUMN_COUNT; j++)
+                {
+                    Mezo mezo = new Mezo(i, j);
+                    mezo.Location = new Point(i * 100, j * 100);
+                    mezo.Click += new EventHandler(mezoClick);
+                    gameField[i, j] = mezo;
+                    Controls.Add(mezo);
+                }
+            }
+        }
+
+        private void mezoClick(object sender, EventArgs e)
+        {
+            Mezo mezo = (Mezo)sender;
+
+            Point point = mezo.Point;
+
+            Operator op = new Operator(point, allapot.Player);
+            if (op.Elofeltetel(allapot))
+            {
+                Kirajzol(mezo);
+                allapot = op.Lepes(allapot);
+                CelfeltetelVizsgalat();
+
+               //TO-DO Negamax
+
+            }
+        }
+
+        private void CelfeltetelVizsgalat()
+        {
+            if (allapot.Celfeltetel() != null)
+            {
+                if (allapot.Celfeltetel() == "Draw")
+                {
+                    MessageBox.Show(allapot.Celfeltetel());
+                }
+                else
+                {
+                    MessageBox.Show("Congratulations! Winner: " + allapot.Celfeltetel());
+                }
+                this.Close();
+                Application.Exit();
+            }
+        }
+
+        private void Kirajzol(Mezo mezo)
+        {
+            mezo.Text = allapot.Player;
+            if (allapot.Player == "X")
+            {
+                mezo.ForeColor = Color.Blue;
+            }
+            else
+            {
+                mezo.ForeColor = Color.Yellow;
+            }
         }
     }
 }
