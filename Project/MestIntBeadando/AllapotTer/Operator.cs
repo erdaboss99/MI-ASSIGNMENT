87dc5c2b7ec921d966503a9047c852632bc3605f
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-05-27 00:48:03 +0200

    Create Operator class

---
+++ Project/MestIntBeadando/AllapotTer/Operator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MestIntBeadando.AllapotTer
+{
+    class Operator
+    {
+        private string what;
+        private Point where;
+        private int weight;
+
+        public string What
+        {
+            get { return what; }
+            set { what = value; }
+        }
+
+        public Point Where
+        {
+            get { return where; }
+            set { where = value; }
+        }
+
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public Operator(Point where, string what)
+        {
+            this.Where = where;
+            this.What = what;
+        }
+
+        public bool Elofeltetel(Allapot aktualisAllapot)
+        {
+            return aktualisAllapot.GameField[Where.X, Where.Y] == null;
+        }
+
+        public Allapot Lepes(Allapot aktualisAllapot)
+        {
+            Allapot ujAllapot = new Allapot();
+            ujAllapot.GameField = (string[,])aktualisAllapot.GameField.Clone();
+
+            ujAllapot.GameField[Where.X, Where.Y] = What;
+            if (What == "X")
+            {
+                ujAllapot.Player = "O";
+            }
+            else
+            {
+                ujAllapot.Player = "X";
+            }
+            return ujAllapot;
+        }
+    }
+}
