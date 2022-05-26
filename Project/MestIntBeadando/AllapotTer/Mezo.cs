2cd75091aa7b251ff5f82b58a68f6f849529ff17
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-05-27 00:48:12 +0200

    Create Mezo class

---
+++ Project/MestIntBeadando/AllapotTer/Mezo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MestIntBeadando.AllapotTer
+{
+    class Mezo : Button
+    {
+        private Point point;
+
+        public Point Point
+        {
+            get { return point; }
+            set { point = value; }
+        }
+
+        public Mezo(int x, int y)
+        {
+            this.Point = new Point(x, y);
+            this.Size = new Size(100, 100);
+            this.Font = new Font(this.Font.FontFamily, 20, FontStyle.Bold);
+        }
+    }
+}
